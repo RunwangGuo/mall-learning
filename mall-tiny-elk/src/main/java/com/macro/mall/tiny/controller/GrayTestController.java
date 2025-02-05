8c87bc12dea6a5d7f1a301b358ce1b93aed1858c
Runwang Guo <shengruxiahuaya@gmail.com>
2025-02-05 17:10:12 +0800

    灰度测试版本A

---
+++ mall-tiny-elk/src/main/java/com/macro/mall/tiny/controller/GrayTestController.java
@@ -0,0 +1,19 @@
+package com.macro.mall.tiny.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "GrayTestController", description = "灰度测试接口")
+@RestController
+@RequestMapping("/grayTest")
+public class GrayTestController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseBody
+    public String grayTest() {
+        return "Gray Test - Version A";
+    }
+}
